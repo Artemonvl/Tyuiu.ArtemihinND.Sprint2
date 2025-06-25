6cf57c4653cbac2329269db97b1a04c57117e085
Tyuiu.ArtemihinND <nikitos.artemihin@mail.ru>
2025-06-26 03:12:33 +0500

    Task0

---
+++ Tyuiu.ArtemihinND.Sprint2.Task0.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ArtemihinND.Sprint2.Task0.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ArtemihinND.Sprint2.Task0.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("891f76ef-c22a-48f3-a419-c0f876a67906")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
