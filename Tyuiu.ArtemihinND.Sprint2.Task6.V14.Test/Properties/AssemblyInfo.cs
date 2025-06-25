43941f10330eb35660c936972b3ac5ada6e75b38
Tyuiu.ArtemihinND <nikitos.artemihin@mail.ru>
2025-06-26 04:16:07 +0500

    Task6

---
+++ Tyuiu.ArtemihinND.Sprint2.Task6.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ArtemihinND.Sprint2.Task6.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ArtemihinND.Sprint2.Task6.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d172d38-76aa-401f-8e0a-fc2b6d04680f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
